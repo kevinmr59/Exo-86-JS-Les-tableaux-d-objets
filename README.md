80bde3244d9c0f4116677729448b37051e3557d7
Nux007 <007.nux@gmail.com>
2020-12-07 06:49:23 +0100

    Exo 86

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Exo-86-JS-Les-tableaux-d-objets
+
+A l'aide du code déjà présent dans le fichier JavaScript:
+
+        1. Ajoutez les voitures peugeot, citroën et ford dans un tableau de l'objet voitures ( propriété collection )
+        2. A l'aide d'une boucle de votre choix, parcourrez les véhicules de la collection
+        3. Dans cette boucle, invoquez pour chaque véhicule la m"thode ouvrirPorte()
+        4. Créez un paragraphe dans le code HTML
+        5. Ajoutez la couleur de chaque voiture dans un nouveau span que vous créerez en JS ( utilisez la propriété couleur )
---
+++ app.js
@@ -0,0 +1,40 @@
+/* Ne touchez pas à ce code */
+let citroen = {
+    color: 'blue',
+    // Méthode ouvrir porte
+    ouvrirPorte: function() {
+        console.log("La porte de la Citroën est ouverte");
+    },
+}
+
+let peugeot = {
+    color: 'yellow',
+    // Méthode ouvrir porte
+    ouvrirPorte: function() {
+        console.log("La porte de la Peugeot est ouverte");
+    }
+}
+
+let ford = {
+    color: 'red',
+    // Méthode ouvrir porte
+    ouvrirPorte: function() {
+        console.log("La porte de la Ford est ouverte");
+    }
+}
+
+
+/**
+ * Votre code commence ici !
+ */
+let voitures = {
+    garage: 'concession',
+    lieu: 'Fourmies',
+    // Dans un tableau, ajouter les différentes voitures que vous possédez.
+    collection: [], // Vos voitures dans cette propriété !
+}
+
+// FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
+// TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+
+
