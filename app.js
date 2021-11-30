01c7200b21c74c5d58bedcc86c0535972c493771
kevinmr59 <kevinmura59610@gmail.com>
2021-11-30 14:09:15 +0100

    tableaux d'objets

--- app.js
+++ app.js
@@ -31,8 +31,15 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
+for (let i = 0; i < voitures.collection.length; i++) {
+    let para = document.getElementById('para');
+    let span = document.createElement('span')
+    span.innerHTML = voitures.collection[i].color + " , " ;
+    span.style.color = voitures.collection[i].color;
+    para.appendChild(span);
+    voitures.collection[i].ouvrirPorte();}
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
